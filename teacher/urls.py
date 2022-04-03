b4b4172ae98395c8c9d943b301c7736bfe1b7ae2
r4g3 <bscortes94@ucatolica.edu.co>
2022-04-03 17:36:29 -0500

    Lista los docentes en la base de datos, junto con un, template para mostrar la information

--- teacher/urls.py
+++ teacher/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import HomeView, ListTeacherView, CreateTeacherView
+from .views import HomeView, ListTeacherView, CreateTeacherView, ShowInfo
 
 app_name = "teacher"
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('list/', ListTeacherView.as_view(), name="list_teacher"),
-    path('create/', CreateTeacherView.as_view(), name="create_teacher")
+    path('create/', CreateTeacherView.as_view(), name="create_teacher"),
+    path('show_info/<int:id_user>/', ShowInfo.as_view(), name="show_info")
 
 ]
